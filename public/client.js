0ea4122e870749499f3c48c1342a45854a9f1239
Glen <mistergjones@yahoo.com.au>
2020-05-20 11:13:34 +1000

    changed order of routes. Changed client.js to location = path

--- public/client.js
+++ public/client.js
@@ -1,51 +1,48 @@
-let pin = null
+let pin = null;
 
 const handlePinClick = (e) => {
     // console.log(e.target)
-    let path = `/beers/${e.target.metadata.id}`
-    axios.get(path).then(res => {
-        console.log(res)
-        document.body.innerHTML = res.data
-    })
-}
+    let path = `/beers/${e.target.metadata.id}`;
+    location = path;
+    // axios.get(path).then(res => {
+    //     console.log(res)
+    //     document.body.innerHTML = res.data
+    // })
+};
 
 function getMap() {
-    var map = new Microsoft.Maps.Map('#map', {
-        center: new Microsoft.Maps.Location(-37.8136, 
-            144.9631),
-        zoom: 9
-    })
-    var center = map.getCenter()
+    var map = new Microsoft.Maps.Map("#map", {
+        center: new Microsoft.Maps.Location(-37.8136, 144.9631),
+        zoom: 9,
+    });
+    var center = map.getCenter();
 
-    const path = '/pubs'
+    const path = "/pubs";
 
-    axios.get(path).then(res => {
+    axios.get(path).then((res) => {
         // console.log(res.data)
-        
-        let pubs = res.data
-        
-        pubs.forEach(pub => {
-            console.log(pub)
-            var location = {latitude: pub.lat, longitude: pub.long}
+
+        let pubs = res.data;
+
+        pubs.forEach((pub) => {
+            console.log(pub);
+            var location = { latitude: pub.lat, longitude: pub.long };
 
             //Create custom Pushpin
             pin = new Microsoft.Maps.Pushpin(location, {
-            title: pub.pubname, 
-            icon: pub.is_pub_ratedb? 'beer.png':'pub-icon.png',
-            anchor: new Microsoft.Maps.Point(12,36)
+                title: pub.pubname,
+                icon: pub.is_pub_ratedb ? "beer.png" : "pub-icon.png",
+                anchor: new Microsoft.Maps.Point(12, 36),
+            });
+
+            pin.metadata = {
+                id: pub.id,
+            };
+
+            Microsoft.Maps.Events.addHandler(pin, "click", handlePinClick);
+
+            //Add the pushpin to the map
+            map.entities.push(pin);
         });
-
-        pin.metadata = {
-            id: pub.id
-        }
-
-        Microsoft.Maps.Events.addHandler(pin, 'click', handlePinClick)
-        
-        //Add the pushpin to the map
-        map.entities.push(pin)
-            
-        });
-        
-    })
-
+    });
 }
--- server.js
+++ server.js
@@ -10,25 +10,14 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+// app.get("/", (req, res ) => {
+//     res.render("filename go here")
+// })
+
 app.get("/pubs", (req, res) => {
     db.query("select * from pub;", (err, dbRes) => {
         res.json(dbRes.rows);
     });
-});
-
-app.get("/beers/:id", (req, res) => {
-    //res.send("Beer info here");
-    console.log(req.params.id);
-    var all_records = null;
-    db.query(
-        "select avg(rating), beertype.beertype, beerbrand.beerbrand from rating, pub,beertype,beerbrand where rating.pub_id = pub.id and pub.id=2 and beertype.id = rating.beertype and beerbrand.id = rating.beerbrand_id group by beertype.beertype,beerbrand.beerbrand;",
-        (err, dbres) => {
-            all_records = dbres.rows;
-            //res.json(dbres.rows);
-            //console.log("Hello");
-            res.render("show", { beerTypes: all_records });
-        }
-    );
 });
 
 app.get("/beers/new", (req, res) => {
@@ -80,6 +69,21 @@ app.get("/beers/new", (req, res) => {
     //     // allBeerTypes: allBeerTypes,
     //     pubInfo: pubInfo,
     // });
+});
+
+app.get("/beers/:id", (req, res) => {
+    //res.send("Beer info here");
+    console.log(req.params.id);
+    var all_records = null;
+    db.query(
+        "select avg(rating), beertype.beertype, beerbrand.beerbrand from rating, pub,beertype,beerbrand where rating.pub_id = pub.id and pub.id=2 and beertype.id = rating.beertype and beerbrand.id = rating.beerbrand_id group by beertype.beertype,beerbrand.beerbrand;",
+        (err, dbres) => {
+            all_records = dbres.rows;
+            //res.json(dbres.rows);
+            //console.log("Hello");
+            res.render("show", { beerTypes: all_records });
+        }
+    );
 });
 
 app.listen(port, () => {
